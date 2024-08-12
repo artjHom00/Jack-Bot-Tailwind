1fb218686163c40a7173c4f862edff1086d81f4b
artyom <agasparyan0000@gmail.com>
2024-08-12 11:08:06 +0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'figmawhite': '#FFF5EA',
+        'figmabeige': '#F0CBA2',
+        'figmayellow': '#F1AC1D',
+        'figmablack': '#1E110D',
+        'figmabrown': '#52433F',
+        'figmared': '#C33F34',
+      }
+    },
+  },
+  plugins: [],
+}
