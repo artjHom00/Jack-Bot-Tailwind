3ae868fcbe6a7aa891d036049cd4a55f48578da8
artyom <agasparyan0000@gmail.com>
2024-08-13 19:36:31 +0400

    pc version without additional elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,17 @@ module.exports = {
       colors: {
         'figmawhite': '#FFF5EA',
         'figmabeige': '#F0CBA2',
+        'figmabeige2': '#AAA49D',
         'figmayellow': '#F1AC1D',
         'figmablack': '#1E110D',
         'figmabrown': '#52433F',
         'figmared': '#C33F34',
-      }
+      },
+      maxWidth: {
+        'screen-3xl': '1600px', // Adjust the values as needed
+        'screen-4xl': '1920px',
+        'screen-5xl': '2240px',
+      },
     },
   },
   plugins: [],
